649d35fe7ef7b463e48677c628edcfc296f6d7f3
Johannes Sattler <johannessattler01@gmail.com>
2021-11-19 11:35:01 +0100

    added faker as dev-depend
    https://www.npmjs.com/package/faker

--- models/User.model.js
+++ models/User.model.js
@@ -11,7 +11,6 @@ const userSchema = new Schema(
       unique: true,
     },
     password: String,
-    totalUpvotes: Number,
     postCreated: Array,
     postUpvoted: Array,
     comments: Array,
