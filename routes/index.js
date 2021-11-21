728b68a2e44a79ab3106307e808035b923682846
Johannes Sattler <johannessattler01@gmail.com>
2021-11-21 15:14:27 +0100

    connected partials to DB model
    inside route /home, for every DB post entry will create post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
+const PostModel = require('../models/Post.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/home", async (req, res, next) => {
+  const posts = await PostModel.find()
+  // create min ago field
+  posts.forEach(post => {
+    post.timeAgo = '2 min ago'
+  })
+
+  res.render("pages/main.hbs", {posts});
+});
+
 
 module.exports = router;
