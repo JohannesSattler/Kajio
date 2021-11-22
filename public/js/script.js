b171ab27e5c71b48c4dcb3bb2e4ca3300a7f0d03
Johannes Sattler <johannessattler01@gmail.com>
2021-11-22 12:06:31 +0100

    Up/Down vote api update

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,10 @@ function addAllEventListeners() {
   };
 }
 
+function checkIfVoted(post) {
+  
+}
+
 function handlePostEvents(post) {
   const uparrow = post.querySelector('#votes-uparrow')
   const downarrow = post.querySelector('#votes-downarrow')
@@ -31,19 +35,15 @@ async function handleVoteClick(event) {
   const downVote = parent.querySelector('#votes-downarrow')
   const upVote = parent.querySelector('#votes-uparrow')
   const counter = parent.querySelector('#votes-amount')
-  let currentCount = Number(counter.innerText)
 
-  console.log(currentCount)
-
+  // chenge up/down vote buttons
   if(upOrDownVote) {
     upVote.classList.add('img-selected', 'img-upvote')
     downVote.classList.remove('img-selected', 'img-downvote')
-    counter.innerText = currentCount+1;
   } 
   else {
     upVote.classList.remove('img-selected', 'img-upvote')
     downVote.classList.add('img-selected', 'img-downvote')
-    counter.innerText = currentCount-1;
   }
 
   const data = await fetch("http://localhost:3000/home/vote", {
@@ -57,6 +57,7 @@ async function handleVoteClick(event) {
       upOrDownVote
     })
   })
-  
-  console.log(data.status)
+
+  const response = await data.json()
+  counter.innerText = JSON.parse(response).votes
 } 
--- routes/API.routes.js
+++ routes/API.routes.js
@@ -6,20 +6,32 @@ const UserModel = require('../models/User.model');
 // Route to upvote and downvote
 router.post("/home/vote", async (req, res, next) => {
   const {userID, postID, upOrDownVote} = req.body
-
   const updatedPost = await PostModel.findById(postID)
 
-  if(upOrDownVote) {
+  // check for up or downvote and check if its includes in upvote or downvote
+  if(upOrDownVote && !updatedPost.upvotes.includes(userID)) {
     updatedPost.upvotes.push(userID)
+
+    // check if its inside downvote
+    if(updatedPost.downvotes.includes(userID)) {
+      const index = updatedPost.downvotes.indexOf(userID)
+      updatedPost.downvotes.splice(index, 1)
+    }
   }
-  else {
+  else if(!upOrDownVote && !updatedPost.downvotes.includes(userID)) {
     updatedPost.downvotes.push(userID)
+
+    // check if its inside upvote
+    if(updatedPost.upvotes.includes(userID)) {
+      const index = updatedPost.upvotes.indexOf(userID)
+      updatedPost.upvotes.splice(index, 1)
+    }
   }
 
-  const final = await PostModel.findByIdAndUpdate(postID, updatedPost)
-  
-  
-  res.status(200).send();
+  await PostModel.findByIdAndUpdate(postID, updatedPost)
+
+  const votes = updatedPost.upvotes.length - updatedPost.downvotes.length
+  res.status(200).json(JSON.stringify({votes}))
 });
 
 module.exports = router;
